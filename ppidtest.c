8a1f255c99f0f1942529eda62f40cdd41c6b1c6b
arya <a.khaligh@aut.ac.ir>
2019-03-26 21:39:02 +0430

    Modify ppidtest file

--- ppidtest.c
+++ ppidtest.c
@@ -2,19 +2,17 @@
 #include "user.h"
 
 int main(void) {
-
   int pid = fork();
 
-
   if(pid > 0){
-  printf(1, "Parent says: (child pid is=%d) \n", pid);
-  pid = wait();
-  printf(1, "Parent says: (child %d is done)\n", pid);
+    printf(1, "Parent says: (child pid is: %d) (my pid is: %d)\n", pid, getpid());
+    pid = wait();
+    printf(1, "Parent says: (child %d is done)\n", pid);
   } else if(pid == 0){
-  printf(1, "Child says: (exiting)\n");
-  printf(1, "Child says: (My parent pid is: %d)", getppid());
-  exit();
+    printf(1, "Child says: (My parent pid is: %d)\n", getppid());
+    printf(1, "Child says: (exiting)\n");
+    exit();
   } else {
-  printf(1, "Fork error\n");
+    printf(1, "Fork error\n");
   }
 }
