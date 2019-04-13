5c3488bfc4214bfa37689c5c3b353f6e42cd1625
Arya Khaligh <bartararya@gmail.com>
2019-04-13 13:40:56 +0430

    Fix 2 threads simultanious threads

--- ppidtest.c
+++ ppidtest.c
@@ -5,8 +5,8 @@ int main(void) {
   int pid = fork();
 
   if(pid > 0){
+    wait();
     printf(1, "Parent says: (child pid is: %d) (my pid is: %d)\n", pid, getpid());
-    pid = wait();
     printf(1, "Parent says: (child %d is done)\n", pid);
   } else if(pid == 0){
     printf(1, "Child says: (My parent pid is: %d)\n", getppid());
@@ -15,4 +15,6 @@ int main(void) {
   } else {
     printf(1, "Fork error\n");
   }
+  
+  exit();
 }
