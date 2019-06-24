b04c0ea32778c8afe9cf00b9037d0be32da865a8
AngryMan0 <hadi.hosseini0171@gmail.com>
2019-06-24 05:54:45 +0430

    GRT done

--- Gsanity.c
+++ Gsanity.c
@@ -4,11 +4,30 @@
 
 int main(){
     int pid;
-    printf(1,"Father pid is %d" ,getpid());
+    printf(1,"Father pid is %d\n" ,getpid());
     //Wait 10 Seconds
     sleep(100);
-    printf(1,"Father is awake");
+    printf(1,"Father is awake\n");
     pid = fork();
+    //Father
+    if(pid  != 0 ){
+        for(int i=0; i<50; i++){
+            printf(1,"Process %d is printing for the %d time\n" , getpid() , i);
+
+        }
+        printf(1,"Finished Parent Work\n");
+        wait();
+    }
+    //Child 
+    else if(pid  == 0 ){
+        for(int i=0; i<50; i++){
+            printf(1,"Process %d is printing for the %d time\n" , getpid() , i);
+
+        }
+        printf(1,"Finished Child Work\n");
+        exit();
+    }
+
     exit();
 	return 0;
 }
