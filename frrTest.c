fa2647fd0fa57e3784c09c78a4bcf8336eace268
Arya <bartararya@gmail.com>
2019-06-25 13:10:06 +0430

    Add frrTest

---
+++ frrTest.c
@@ -0,0 +1,54 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "stdbool.h"
+
+void rrtest(void)
+{
+    int pid;
+    int N = 10;
+    bool lock = false;
+    long long int wTime, rTime;
+
+    for (int n = 0; n < N; n++)
+    {
+        pid = fork();
+        if (pid < 0)
+        {
+            return;
+        }
+        if (pid == 0)
+        {
+            int i;
+            for (i = 0; i < 400; i++)
+            {   
+                while(lock);
+                lock=true;
+                printf(1, " [%d] ", getpid(), i);
+                lock=false;
+            }
+            break;
+        }
+    }
+    if (pid != 0)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            wait();
+        }
+    }
+
+    while(lock);
+    lock=true;
+    getPerformanceData(&wTime, &rTime);
+    // printf(1, "\n\n [FINISHED pid: %d | ", getpid());
+    // printf(1, "wTime: %d | ", wTime);
+    // printf(1, "rTime: %d] \n\n", rTime);
+    lock=false;
+}
+
+int main(void)
+{
+    rrtest();
+    exit();
+}
